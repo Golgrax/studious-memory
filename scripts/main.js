f154a40ca67cd800daef6029d7d332981fd0e0ef
Golgrax <pepe.kotelurow@gmail.com>
2025-08-08 12:54:00 +0800

    fix: remove unused method + use CDN fonts & icons

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@ class BayanihanWeatherApp {
         this.init = this.init.bind(this);
         this.loadAlerts = this.loadAlerts.bind(this);
         this.updateDisplay = this.updateDisplay.bind(this);
-        this.handleFilterChange = this.handleFilterChange.bind(this);
+        // this.handleFilterChange = this.handleFilterChange.bind(this);
     }
 
     /**
